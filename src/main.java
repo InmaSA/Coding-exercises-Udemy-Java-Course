f128a2a39f51cd07dc891d5a9cd8923c395ff320
Inma <inmanewmail@gmail.com>
2019-10-23 11:17:07 +0200

    SimpleCalculator and Person done

--- src/main.java
+++ src/main.java
@@ -1,28 +1,37 @@
 public class main {
 
     public static void main(String[] args) {
-        MegaBytesConverter.printMegaBytesAndKiloBytes(-1024);
-        System.out.println(AreaCalculator.area(0.0));
-        MinutesToYearsDaysCalculator.printYearsAndDays(- 525600);
-        IntEqualityPrinter.printEqual(1,2,1);
-        System.out.println(PlayingCat.isCatPlaying(false, 35));
-        System.out.println(NumberOfDaysInMonth.getDaysInMonth(1, 2020));
-        System.out.println(SumOddRange.sumOdd(100,00));
 
-        System.out.println(NumberPalindrome.isPalindrome(-1221));
-        System.out.println(NumberPalindrome.isPalindrome(-11212));
+        SimpleCalculator calculator = new SimpleCalculator();
 
-        System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(10));
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
 
-        System.out.println(EvenDigitSum.getEvenDigitSum(252));
+        System.out.println("add = " + calculator.getAdditionResult());
+        System.out.println("subtract = " + calculator.getSubtractionResult());
 
-        System.out.println(SharedDigit.hasSharedDigit(12, 13));
-        System.out.println("***********************");
-        System.out.println(LastDigitChecker.hasSameLastDigit(90, 97, 999));
-        System.out.println("***********************");
-        System.out.println(GreatestCommonDivisor.getGreatestCommonDivisor(81, 153));
-        System.out.println("***********************");
-        InputCalculator.inputThenPrintSumAndAverage();
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
 
+        System.out.println("multiply = " + calculator.getMultiplicationResult());
+        System.out.println("divide = " + calculator.getDivisionResult());
+
+
+        Person person = new Person();
+
+        person.setFirstName("");
+        person.setLastName("");
+        person.setAge(10);
+
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen " + person.isTeen());
+
+        person.setFirstName("John");
+        person.setAge(18);
+
+        System.out.println("teen " + person.isTeen());
+
+        person.setLastName("Smith");
+        System.out.println("fullName= " + person.getFullName());
     }
 }
