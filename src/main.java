af47bb68a7ee17e2281c4cd3dcf959ccf5bc738e
Inma <inmanewmail@gmail.com>
2019-10-24 15:46:16 +0200

    Wall

--- src/main.java
+++ src/main.java
@@ -2,36 +2,14 @@ public class main {
 
     public static void main(String[] args) {
 
-        SimpleCalculator calculator = new SimpleCalculator();
+       Wall wall = new Wall(5,4);
 
-        calculator.setFirstNumber(5.0);
-        calculator.setSecondNumber(4);
+        System.out.println("area = " + wall.getArea());
 
-        System.out.println("add = " + calculator.getAdditionResult());
-        System.out.println("subtract = " + calculator.getSubtractionResult());
+        wall.setHeight(-1.5);
 
-        calculator.setFirstNumber(5.25);
-        calculator.setSecondNumber(0);
-
-        System.out.println("multiply = " + calculator.getMultiplicationResult());
-        System.out.println("divide = " + calculator.getDivisionResult());
-
-
-        Person person = new Person();
-
-        person.setFirstName("");
-        person.setLastName("");
-        person.setAge(10);
-
-        System.out.println("fullName= " + person.getFullName());
-        System.out.println("teen " + person.isTeen());
-
-        person.setFirstName("John");
-        person.setAge(18);
-
-        System.out.println("teen " + person.isTeen());
-
-        person.setLastName("Smith");
-        System.out.println("fullName= " + person.getFullName());
+        System.out.println("width = " + wall.getWidth());
+        System.out.println("height = " + wall.getHeight());
+        System.out.println("area = " + wall.getArea());
     }
 }
