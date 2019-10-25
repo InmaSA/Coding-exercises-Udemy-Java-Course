4aabc96e37ebdb4a23518c5c692a89e5c4ac4607
Inma <inmanewmail@gmail.com>
2019-10-25 20:30:18 +0200

    Point

--- src/main.java
+++ src/main.java
@@ -2,14 +2,14 @@ public class main {
 
     public static void main(String[] args) {
 
-       Wall wall = new Wall(5,4);
+       Point first = new Point(6,5);
+       Point second = new Point(3,1);
 
-        System.out.println("area = " + wall.getArea());
+        System.out.println("distance(0,0) = " + first.distance());
+        System.out.println("distance(second) = " + first.distance(second));
+        System.out.println("distance(2,2) = " + first.distance(2,2));
 
-        wall.setHeight(-1.5);
-
-        System.out.println("width = " + wall.getWidth());
-        System.out.println("height = " + wall.getHeight());
-        System.out.println("area = " + wall.getArea());
+        Point point = new Point();
+        System.out.println("distance() = " + point.distance());
     }
 }
